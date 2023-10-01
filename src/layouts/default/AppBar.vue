282738f97bb6a672a38f9d668a17c51a225ba07d
Ahmosys <ahmosyspro@protonmail.com>
2023-10-01 15:42:59 +0200

    feat: add custom theme & add appbar

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,10 +1,11 @@
 <template>
-  <v-app-bar flat>
+  <v-app-bar scroll-behavior="elevate" color="primary">
     <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
-
-      Essentials Preset
+      <v-icon icon="mdi-wallpaper" />
+      Nordpapers
     </v-app-bar-title>
+    <v-btn icon="mdi-github" href="https://github.com/Ahmosys/nordpapers"></v-btn>
+    <v-btn icon="mdi-information-outline"></v-btn>
   </v-app-bar>
 </template>
 
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,16 +11,28 @@ import 'vuetify/styles'
 // Composables
 import { createVuetify } from 'vuetify'
 
+// Custom theme properties
+// https://vuetifyjs.com/en/features/theme/#custom-properties
+const customNordTheme = {
+  dark: true,
+  colors : {
+    primary: '#5e81ac',
+    secondary: '#88c0d0',
+    error: '#bf616a',
+    info: '#ebcb8b',
+    success: '#a3be8c',
+    warning: '#d08770',
+    background: '#2e3440',
+    surface: '#3b4252',
+  },
+}
+
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
+    defaultTheme: 'customNordTheme',
     themes: {
-      light: {
-        colors: {
-          primary: '#1867C0',
-          secondary: '#5CBBF6',
-        },
-      },
+      customNordTheme,
     },
   },
-})
+});
