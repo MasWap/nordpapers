42075c473199798cb95aebb1b53df78959b90d43
Ahmosys <ahmosyspro@protonmail.com>
2023-10-02 00:04:03 +0200

    feat: add disclaimer dialog

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -4,20 +4,42 @@
       <v-icon icon="mdi-wallpaper" />
       Nordpapers
     </v-app-bar-title>
-    <v-btn :icon="theme.global.current.value.dark ? 'mdi-weather-night' : 'mdi-weather-sunny'" @click="toggleTheme"></v-btn>
+    <v-btn :icon="theme.global.current.value.dark ? 'mdi-weather-night' : 'mdi-weather-sunny'"
+      @click="toggleTheme"></v-btn>
     <v-btn icon="mdi-github" href="https://github.com/Ahmosys/nordpapers" target="_blank"></v-btn>
-    <v-btn icon="mdi-information-outline"></v-btn>
+    <v-btn icon="mdi-information-outline" @click="toggleDialog"></v-btn>
+    <v-dialog v-model="dialog" width="512">
+      <v-card title="Disclaimer" prepend-icon="mdi-information-outline">
+        <v-card-text>
+          I want to clarify that I do not assert any ownership of the wallpapers featured in this repository. The majority
+          of these wallpapers were discovered on platforms such as Reddit. If you happen to
+          identify any images in this repository that belong to you and have limited utility here, please inform me, and I
+          will promptly remove them.
+        </v-card-text>
+        <v-card-actions>
+          <v-btn variant="tonal" block @click="toggleDialog">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-app-bar>
 </template>
 
 <script setup>
-  import { useTheme } from 'vuetify'
+import { ref } from 'vue';
+import { useTheme } from 'vuetify'
 
-  const theme = useTheme()
+let dialog = ref(false)
+const theme = useTheme()
 
-  function toggleTheme() {
-    theme.global.name.value = theme.global.current.value.dark ? 'customLightNordTheme' : 'customDarkNordTheme';
-  }
+function toggleTheme() {
+  // Handles theme change based on current theme
+  theme.global.name.value = theme.global.current.value.dark ? 'customLightNordTheme' : 'customDarkNordTheme';
+}
+
+function toggleDialog() {
+  // Handles dialog visibility
+  dialog.value = !dialog.value
+}
 </script>
 
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
-  <hello-world />
+  <v-row class="my-5">
+      <v-col cols="12" class="text-center">
+        <p class="text-h2 font-weight-bold">Nordpapers</p>
+        <p class="text-h5 font-weight-light mt-1">A collection of walls with Nordic colors <v-icon
+            icon="mdi-palette"></v-icon></p>
+      </v-col>
+  </v-row>
+  <gallery-image />
 </template>
 
 <script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+  import GalleryImage from '@/components/GalleryImage.vue'
 </script>
