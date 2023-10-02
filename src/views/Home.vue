348996358fa095f4d721bfda01cea177123ec83a
Ahmosys <ahmosyspro@protonmail.com>
2023-10-02 19:58:31 +0200

    fix: overflow caused by main title

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,16 @@
 <template>
-  <v-row class="my-5">
+  <v-container>
+    <v-row class="my-5">
       <v-col cols="12" class="text-center">
         <p class="text-h2 font-weight-bold">Nordpapers</p>
         <p class="text-h5 font-weight-light mt-1">A collection of walls with Nordic colors <v-icon
             icon="mdi-palette"></v-icon></p>
       </v-col>
-  </v-row>
+    </v-row>
+  </v-container>
   <gallery-image />
 </template>
 
 <script setup>
-  import GalleryImage from '@/components/GalleryImage.vue'
+import GalleryImage from '@/components/GalleryImage.vue'
 </script>
