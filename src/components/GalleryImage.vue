f9f4429ec1deec77a8fa86809b995f2947abe4d8
Ahmosys <ahmosyspro@protonmail.com>
2023-10-03 22:44:28 +0200

    feat: add footer

--- src/components/GalleryImage.vue
+++ src/components/GalleryImage.vue
@@ -1,9 +1,9 @@
 <template>
   <v-container>
     <v-row>
-      <v-col v-for="n in 100" cols="12" sm="6" md="4" lg="3" xl="3">
+      <v-col v-for="n in 24" cols="12" sm="6" md="4" lg="3" xl="3">
         <v-img :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-          :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`" aspect-ratio="16/9" cover class="bg-grey-lighten-2">
+          :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`" :aspect-ratio="16 / 9" cover>
           <template v-slot:placeholder>
             <v-row class="fill-height ma-0" align="center" justify="center">
               <v-progress-circular indeterminate color="grey-lighten-5"></v-progress-circular>
---
+++ src/layouts/default/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-footer class="text-center d-flex flex-column mt-5">
+    <div>
+      <v-btn class="mx-4" icon="mdi-github" variant="text"></v-btn>
+    </div>
+    <div class="pt-0">
+      The images have not been created by me, they have mainly been gathered from Reddit and other origins.
+    </div>
+    <v-divider></v-divider>
+    <div>
+      {{ new Date().getFullYear() }} — <strong>Nordpapers</strong>
+    </div>
+  </v-footer>
+</template>
+
+<script setup>
+//
+</script>
