265b257e4b8ad336ce682530e38c06fe0bc3df53
Ahmosys <ahmosyspro@protonmail.com>
2023-10-04 18:38:59 +0200

    refactor: split component in two components

--- src/components/GalleryImage.vue
+++ src/components/GalleryImage.vue
@@ -10,30 +10,7 @@
         lg="3"
         xl="3"
       >
-        <v-card
-          elevation="10"
-          :href="image.download_url"
-          :border="10"
-          target="_blank"
-        >
-          <v-img
-            class="elevation-15"
-            :src="getImageProxyUrl(image.download_url)"
-            :aspect-ratio="16 / 9"
-            :lazy-src="getMinimizedImageProxyUrl(image.download_url)"
-            cover
-            v-bind="props"
-          >
-            <template v-slot:placeholder>
-              <v-row class="fill-height ma-0" align="center" justify="center">
-                <v-progress-circular
-                  indeterminate
-                  color="grey-lighten-5"
-                ></v-progress-circular>
-              </v-row>
-            </template>
-          </v-img>
-        </v-card>
+        <gallery-image-card :image="image" />
       </v-col>
     </v-row>
   </v-container>
@@ -41,29 +18,13 @@
 
 <script setup>
 import { onMounted, ref } from "vue";
+import GalleryImageCard from "@/components/GalleryImageCard.vue";
 
 const REPOSITORY = "ahmosys/nordpapers";
 const IMAGES_DIRECTORY = "public/images";
 const GITHUB_API_URL = `https://api.github.com/repos/${REPOSITORY}/contents/${IMAGES_DIRECTORY}`;
-const IMG_PROXY_URL_FULL =
-  "https://dc1imgproxy.fly.dev/x/rs:auto:332:200:1/plain/";
-const IMG_PROXY_URL_MIN = "https://dc1imgproxy.fly.dev/x/rs:auto:10:6:1/plain/";
 
 const images = ref([]);
-
-/**
- * Get the URL of an image with a full-sized proxy.
- * @param {string} baseUrl - The base URL of the image.
- * @returns {string} - The URL of the image with the imgproxy.
- */
-const getImageProxyUrl = (baseUrl) => `${IMG_PROXY_URL_FULL}${baseUrl}`;
-
-/**
- * Get the URL of a minimized image with a proxy.
- * @param {string} baseUrl - The base URL of the image.
- * @returns {string} - The URL of the minimized image via imgproxy.
- */
-const getMinimizedImageProxyUrl = (baseUrl) => `${IMG_PROXY_URL_MIN}${baseUrl}`;
 
 /**
  * Fetch files from repository folder and retrieve only images via GitHub API.
